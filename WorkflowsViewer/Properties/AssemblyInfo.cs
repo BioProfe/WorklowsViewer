10ded3a0cebc64138c276baac7bff0134ff3b498
BioProfe <53934668+BioProfe@users.noreply.github.com>
2019-09-03 08:08:36 +0200

    Update AssemblyInfo.cs

--- WorkflowsViewer/Properties/AssemblyInfo.cs
+++ WorkflowsViewer/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("BioProfe")]
 [assembly: AssemblyProduct("WorkflowsViewer")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © BioProfe 2019")]
 [assembly: AssemblyTrademark("BioProfe")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.2")]
+[assembly: AssemblyFileVersion("1.0.0.2")]
